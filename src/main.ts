940e9b5e317b0071beef3b0f095a2b7f3ebb05d5
tiesen243 <ttien56906@gmail.com>
2024-04-02 02:22:50 +0700

    dasd

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,8 @@
 import App from '@/app'
 import '@/globals.css'
-import { changeTheme } from '@/lib'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = App({ pathName: window.location.pathname })
 
-const links = document.querySelectorAll<HTMLAnchorElement>('a')
-links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault()
-    history.pushState({}, '', link.href)
-    document.querySelector<HTMLDivElement>('#app')!.innerHTML = App({ pathName: link.pathname })
-    changeTheme(document.querySelector<HTMLButtonElement>('.theme-btn')!)
-  })
+window.addEventListener('popstate', () => {
+  document.querySelector<HTMLDivElement>('#app')!.innerHTML = App({ pathName: window.location.pathname })
 })
