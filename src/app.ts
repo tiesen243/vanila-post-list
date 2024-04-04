f7a5100bcc84651cda2fda11afac85a192d19717
tiesen243 <ttien56906@gmail.com>
2024-04-04 22:36:51 +0700

    dasd

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,10 @@ import { Header } from '@/components/header'
 
 // Routes
 import routes from '@/routes'
+import { html } from './lib/utils'
 
-const App = (props: { pathName: string }): string => `
-  ${Header({ pathName: props.pathName })} 
-  ${routes(props.pathName)} 
-  ${Footer()}
-  `
+const App = (props: { pathName: string }): string => html`
+  ${Header({ pathName: props.pathName })} ${routes(props.pathName)} ${Footer()}
+`
 
 export default App
